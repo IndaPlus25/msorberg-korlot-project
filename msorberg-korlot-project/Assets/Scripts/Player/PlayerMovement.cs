ba6274c1216fd715932307641df8a08a537261dc
MineSwe <marcussorberg@gmail.com>
2026-04-09 23:16:17 +0200

    Add sprites, add some animations

--- msorberg-korlot-project/Assets/Scripts/Player/PlayerMovement.cs
+++ msorberg-korlot-project/Assets/Scripts/Player/PlayerMovement.cs
@@ -8,6 +8,7 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D rb;
     private Vector2 moveInput;
     private Animator anim;
+    float desiredRotation = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -19,24 +20,36 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         rb.linearVelocity = moveInput * moveSpeed;
+        if (!anim.GetCurrentAnimatorStateInfo(0).IsName("PlayerAttack"))
+        {
+            transform.localEulerAngles = new Vector3(0, 0, -desiredRotation);
+        }
     }
 
     public void Move(InputAction.CallbackContext context)
     {
         moveInput = context.ReadValue<Vector2>();
+        if (moveInput.x == 0 && moveInput.y != 0)
+        {
+            anim.SetBool("WalkingDown", true);
+        }
+        else if (moveInput.x != 0)
+        {
+            anim.SetBool("WalkingDown", false);
+            if (moveInput.x > 0)
+            {
+                transform.localEulerAngles = new Vector3(0, 90, 0);
+            }
+        }
     }
 
     public void Look(InputAction.CallbackContext context)
     {
-        if (!anim.GetCurrentAnimatorStateInfo(0).IsName("PlayerAttack"))
+        Vector2 lookVector = context.ReadValue<Vector2>();
+        if (lookVector.x == 0 && lookVector.y == 0)
         {
-            Vector2 lookVector = context.ReadValue<Vector2>();
-            if (lookVector.x == 0 && lookVector.y == 0)
-            {
-                return;
-            }
-            float rotation = Mathf.Atan2(lookVector.x, lookVector.y) * Mathf.Rad2Deg;
-            transform.localEulerAngles = new Vector3(0, 0, -rotation);
+            return;
         }
+        desiredRotation = Mathf.Atan2(lookVector.x, lookVector.y) * Mathf.Rad2Deg;
     }
 }
